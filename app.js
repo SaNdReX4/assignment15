0ee885f7bdb20d988d9e28aa7d7dbb1f26705bd2
SaNdReX4 <sandro.biwkinashvili@gmail.com>
2025-11-09 23:19:17 +0400

    damtavrebuli

---
+++ app.js
@@ -0,0 +1,33 @@
+let myString = "Hello, world!"; // ეს არის string
+console.log(myString);
+let myNumber = 42; //ეს არი ციფრები number
+console.log(myNumber);
+let myBoolean = true; // ეს არის boolean ანუ როცა მცდარი და სწორი გვაქვს (true და false )
+console.log(myBoolean);
+let myBoolean2 = false; // ეს არის boolean ანუ როცა მცდარი და სწორი გვაქვს (true და false )
+console.log(myBoolean2);
+let myNull = null; // null
+console.log(myNull);
+let myUndefined; // ეს არის არაფერი რო არ გივწერია  undefined
+console.log(myUndefined);
+let mySymbol = Symbol("id"); // სიმბოლოებისთვის არის ეს  symbol
+console.log(mySymbol);
+let myBigInt = 123456789101112131415161718192021222324252627282930; // აქ ვწერთ ბევრ რიცხვებს ერთად  bigint
+console.log(myBigInt);
+
+let MyName = "sandro";
+let dayOfWeek = "Sunday";
+let favoriteActivity = "coding";
+
+let shedegi1 =
+  "Hi, my name is " +
+  MyName +
+  "! Today is " +
+  dayOfWeek +
+  ", a perfect day for " +
+  favoriteActivity +
+  ".";
+console.log(shedegi1);
+
+let shedegi2 = `Hi, my name is ${MyName}! Today is ${dayOfWeek}, a perfect day for ${favoriteActivity}.`;
+console.log(shedegi2);
